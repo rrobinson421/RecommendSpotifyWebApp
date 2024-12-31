8c4be9b07576bcbc250bd3035b8bfe1e86264484
rrobinson421 <ryanrobinson421@gmail.com>
2024-12-31 10:02:43 -0500

    Deprecated API Endpoints: - Trial and error with spotipy and SpotifyAPI recommendations don't seem to work anymore - Attempting alternate methods for recommendation features

--- webapp/recommend/main.py
+++ webapp/recommend/main.py
@@ -3,7 +3,6 @@ import spotipy
 import recommend.global_var
 import base64
 import requests
-import json
 
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
@@ -17,23 +16,6 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=YOUR_APP_CLIENT_ID,
                                                client_secret=YOUR_APP_CLIENT_SECRET,
                                                redirect_uri=YOUR_APP_REDIRECT_URI,
                                                scope="user-library-read"))
-
-def get_token_basic():
-    auth_header = base64.b64encode(f"{YOUR_APP_CLIENT_ID}:{YOUR_APP_CLIENT_SECRET}".encode()).decode()
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": f"Basic {auth_header}"
-    }
-    data = {
-        "grant_type": "client_credentials"
-    }
-    response = requests.post(url, headers=headers, data=data)
-    response.raise_for_status()
-    return response.json().get("access_token")
-
-##TODO: Set up Guest Account Authentication
-##TODO: Remove testing methods
-##TODO: Update Nav Bar for Login support
 
 def test_for_artist(name):
     results = sp.search(q='artist:' + name, type='artist', limit=1)
@@ -154,11 +136,24 @@ def apply_recommendations():
     if get_check_3() == True:
         recommend.global_var.cached_recommendations = recommend.global_var.recommendations
     if get_check_1() == True:
-        ##recommend.global_var.recommendations = get_recommendations_default(seed_artists, seed_tracks, seed_genres, get_amount(), filter)
-        recommend.global_var.recommendations = get_recommendations_test()
+        recommend.global_var.recommendations = get_recommendations_default(seed_artists, seed_tracks, seed_genres, get_amount(), filter)
         print(recommend.global_var.recommendations)
     else:
         recommend.global_var.recommendations = "None"
+
+##Spotify Method
+def get_token_basic():
+    auth_header = base64.b64encode(f"{YOUR_APP_CLIENT_ID}:{YOUR_APP_CLIENT_SECRET}".encode()).decode()
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": f"Basic {auth_header}"
+    }
+    data = {
+        "grant_type": "client_credentials"
+    }
+    response = requests.post(url, headers=headers, data=data)
+    response.raise_for_status()
+    return response.json().get("access_token")
 
 def get_recommendations_default(seed_artists, seed_tracks, seed_genres, lim, filter):
     if not recommend.global_var.logged_in:
@@ -170,25 +165,31 @@ def get_recommendations_default(seed_artists, seed_tracks, seed_genres, lim, fil
         "Authorization": f"Bearer {token}"
     }
     params = {
-        "seed_artists": ",".join(seed_artists) if seed_artists and seed_artists != '' else None,
-        "seed_tracks": ",".join(seed_tracks) if seed_tracks and seed_tracks != '' else None,
+        "seed_artists": ",".join(seed_artists) if seed_artists and seed_artists != [''] else None,
+        "seed_tracks": ",".join(seed_tracks) if seed_tracks and seed_tracks != [''] else None,
         "seed_genres": ",".join(seed_genres) if seed_genres else None,
         "limit": lim
     }
     recommendation_json = requests.get(url, headers=headers, params=params)
-    recommendation_json.raise_for_status()
-    return parse_json(recommendation_json, filter)
+    recommendation_json.raise_for_status() 
 
-def get_recommendations_test():
-    filter = 'song'
-    token = get_token_basic()
-    url = "https://api.spotify.com/v1/recommendations?seed_artists=&seed_tracks=4TGZTPJ50VXfqCIEwf6Bmm&limit=5"
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
-    rec_test = requests.get(url, headers=headers)
-    rec_test.raise_for_status()
-    return parse_json(rec_test, filter)
+##Spotipy Method
+def get_recommendations_default(seed_artists, seed_tracks, seed_genres, lim, filter):
+    if not seed_artists or seed_artists == ['']: seed_artists = None
+    if not seed_tracks or seed_tracks == ['']: seed_tracks = None
+    if not seed_genres or seed_genres == ['']: seed_genres = None
+    ##results = sp.recommendations(seed_artists=seed_artists, seed_tracks=seed_tracks, seed_genres=seed_genres, limit=lim)
+    results = sp.recommendations(seed_artists=[get_artist_id()])
+
+    if filter == 'artist':
+        return "artist"
+    elif filter == 'song':
+        return "song"
+    else:
+        return "album"
     
-def parse_json(json, filter):
-    return "got json"
+##last.fm Method
+##https://www.last.fm/api/show/track.getSimilar
+
+##TODO: Check if songs also exist on last.fm
+##TODO: Find recommendations using last.fm recommendation API
